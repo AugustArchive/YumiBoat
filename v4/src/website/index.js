2f57afce1a866c65b4bb7e538f42217f1df027fa
August (Chris) <27751995+ohlookitsAugust@users.noreply.github.com>
2017-09-19 19:07:14 -0700

    Main shit.

---
+++ v4/src/website/index.js
@@ -0,0 +1,25 @@
+let express = require("express");
+
+let app = express();
+
+app.use((req, res, next) => {
+	console.log(req.method + " " + req.url);
+	next();
+});
+
+app.get('/yumiboat/invite', (req, res) => {
+   res.redirect("https://discordapp.com/oauth2/authorize?client_id=317145148901556234&scope=bot&permissions=271633518");
+});
+
+app.use(express.static(`${__dirname}/static`));
+
+app.use("*", (req, res) => {
+	res.status(404).sendFile(`${__dirname}/static/404.html`);
+});
+
+app.listen(80, (err) => {
+	if (err) return throw new Error(err);
+	console.log("Listening on Port 80");
+});
+
+/* Meh. Credit: https://github.com/PassTheMayo/passthemayo.space/blob/master/index.js */
