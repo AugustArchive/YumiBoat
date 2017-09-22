7dd5d3677e19cad7ee578f5bdd9ed2e984d79bef
August (Chris) <27751995+ohlookitsAugust@users.noreply.github.com>
2017-09-22 09:52:30 -0700

    Update prefix.js

--- v4/src/Commands/config/prefix.js
+++ v4/src/Commands/config/prefix.js
@@ -1,1 +1,15 @@
+const Command = require('../../Structures/Command');
+let sql = require('sqlite');
+sql.open('../../Database/Yumi');
 
+class PrefixCommand extends Command {
+   constructor() {
+       super(); 
+   }
+  
+  async run(msg, params) {
+    
+  }
+}
+
+module.exports = PrefixCommand;
